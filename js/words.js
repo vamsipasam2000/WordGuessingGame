711c88dedb098947ad0f2792b05fd2eac3213f62
Vamsi Krishna Pasam <vamsikrishnapasam@Vamsis-MBP.vyvebroadband.net>
2023-10-09 18:19:13 -0400

    add

---
+++ js/words.js
@@ -0,0 +1,303 @@
+let wordList1 = [
+    {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "c",
+        hint: "Stacks and heaps are part of its memory management"
+        
+    },
+    {
+        word: "typescript",
+        hint: "JavaScript's main competitor in web scripting"
+        
+    },
+    {
+        word: "r",
+        hint: "Popular language for data analysis and statistics"
+        
+    },
+    {
+        word: "swift",
+        hint: "Programming language for creating ios applications"
+        
+    },
+    {
+        word: "java",
+        hint: "Developed by Sun Microsystems, now owned by Oracle"
+        
+    },
+    {
+        word: "modular",
+        hint: "A concept that refers to breaking a complex problems into smaller, more manageable parts"
+        
+    },
+    {
+        word: "ruby",
+        hint: "Scripting language with a focus on simplicity"
+        
+    },
+    {
+        word: "haskell",
+        hint: "Functional language known for its strong typing"
+        
+    },
+    {
+        word: "nodejs",
+        hint: "Used extensively for web development, especially on the server-side"
+        
+    },]
+    
+let wordList2=[ 
+    {
+        word: "windows",
+        hint: "Popular operating system by Microsoft"
+    },
+    {
+        word: "photoshop",
+        hint: "Adobe's photo editing software"
+        
+    },
+    {
+        word: "zoom",
+        hint: "Video conferencing software"
+        
+    },
+    {
+        word: "chrome",
+        hint: "Web browser developed by Google"
+        
+    },
+    {
+        word: "office",
+        hint: "Microsoft's suite of office productivity software"
+        
+    },
+    {
+        word: "libreoffice",
+        hint: "Open-source office suite with a penguin mascot"
+        
+    },
+    {
+        word: "whatsapp",
+        hint: "Messaging app with a green logo, now owned by Facebook"
+        
+    },
+    {
+        word: "icloud",
+        hint: "Cloud storage service by Apple"
+        
+    },
+    {
+        word: "git",
+        hint: "Version control system for code collaboration"
+        
+    },
+    {
+        word: "excel",
+        hint: "spreadsheet application that is part of the Microsoft Office suite"
+        
+    },]
+
+let wordList3=[
+    {
+        word: "iphone",
+        hint: "Apple's flagship smartphone"
+    },
+    {
+        word: "playstation",
+        hint: "Gaming console by Sony, known for its exclusives"
+        
+    },
+    {
+        word: "nvidia",
+        hint: "Leading manufacturer of graphics processing units"
+        
+    },
+    {
+        word: "alienware",
+        hint: "High-end gaming PC manufacturer"
+        
+    },
+    {
+        word: "core",
+        hint: "Popular line of processors by Intel"
+        
+    },
+    {
+        word: "seagate",
+        hint: "Leading manufacturer of computer hard drives"
+        
+    },
+    {
+        word: "samsung",
+        hint: "South Korean electronics giant, known for phones and TVs"
+        
+    },
+    {
+        word: "razer",
+        hint: "Gaming hardware company known for its mice and keyboards"
+        
+    },
+    {
+        word: "hp",
+        hint: "American multinational corporation known for printers"
+        
+    },
+    {
+        word: "lenovo",
+        hint: "Chinese multinational company, the world's largest PC vendor"
+        
+    },]
+
+    let wordList4=[{
+        word: "twitter",
+        hint: "A platform known for its 140-character limit"
+    },
+    {
+        word: "facebook",
+        hint: "Owned by Mark Zuckerberg, it's the largest social network"
+        
+    },
+    {
+        word: "tiktok",
+        hint: "Short-form video app that gained popularity in 2020"
+        
+    },
+    {
+        word: "linkedin",
+        hint: "Professional networking platform for job seekers"
+        
+    },
+    {
+        word: "pinterest",
+        hint: "Image-centric platform with pins and boards"
+        
+    },
+    {
+        word: "snapchat",
+        hint: "Popular messaging app known for its disappearing messages"
+        
+    },
+    {
+        word: "youtube",
+        hint: "Video-sharing platform where creators can earn money"
+        
+    },
+    {
+        word: "oculus",
+        hint: "Owned by Meta Platforms, it's a virtual reality social platform"
+        
+    },
+    {
+        word: "instagram",
+        hint: "Photo and video sharing app with Stories feature"
+        
+    },
+    {
+        word: "tumblr",
+        hint: "Microblogging platform focused on multimedia content"
+        
+    },]
+
+let wordList5=[
+    {
+        word: "tony",
+        hint: "Billionaire genius with a suit"
+    },
+    {
+        word: "thor",
+        hint: "Mighty god of thunder"
+        
+    },
+    {
+        word: "Red and blue wall-crawler",
+        hint: "spidey"
+        
+    },
+    {
+        word: "Wakandan superhero and king",
+        hint: "tchalla"
+        
+    },
+    {
+        word: "Green, gamma-powered giant",
+        hint: "hulk"
+        
+    },
+    {
+        word: "Sorcerer Supreme",
+        hint: "strange"
+        
+    },
+    {
+        word: "Mutant with optic blasts",
+        hint: "cyclo"
+        
+    },
+    {
+        word: "Norse trickster and mischief-maker",
+        hint: "loki"
+        
+    },
+    {
+        word: "Feline superheroine with claws",
+        hint: "logan"
+        
+    },
+    {
+        word: "Super-soldier from World War II",
+        hint: "cap"
+        
+    },]
+
+let wordList6=[{
+    word: "tenet",
+    hint: "Nolan Movie"
+    },
+{
+    word: "batman",
+    hint: "super hero"
+    
+},]
+
+let wordList7=[{
+    word: "tenet",
+    hint: "Nolan Movie"
+    },
+{
+    word: "batman",
+    hint: "super hero"
+    
+},]
+
+let wordList8=[{
+    word: "tenet",
+    hint: "Nolan Movie"
+    },
+{
+    word: "batman",
+    hint: "super hero"
+    
+},]
+
+let wordList9=[{
+    word: "tenet",
+    hint: "Nolan Movie"
+    },
+{
+    word: "batman",
+    hint: "super hero"
+    
+},]
+
+let wordList10=[{
+    word: "tenet",
+    hint: "Nolan Movie"
+    },
+{
+    word: "batman",
+    hint: "super hero"
+    
+},]
