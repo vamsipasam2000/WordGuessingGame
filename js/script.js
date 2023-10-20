3b1184dd536bc56c29cee75dbde965e7a2801175
Vamsi Krishna Pasam <vamsikrishnapasam@Vamsis-MacBook-Pro.local>
2023-10-19 21:00:36 -0400

    add

---
+++ js/script.js
@@ -0,0 +1,349 @@
+const inputs = document.querySelector(".inputs"),
+hintTag = document.querySelector(".hint span"),
+guessLeft = document.querySelector(".guess-left span"),
+wrongLetter = document.querySelector(".wrong-letter span"),
+resetBtn = document.querySelector(".reset-btn"),
+typingInput = document.querySelector(".typing-input");
+const wordToPicture = {
+    python: "python.png",
+    guitar: "guitar.jpg",
+    jackie: "jackie.jpg",
+    c: "c.png",
+    typescript: "typescript.png",
+    r: "r.png",
+    swift: "swift.png",
+    java: "java.png",
+    modular: "modular.jpeg",
+    ruby: "ruby.png",
+    haskell: "haskell.png",
+    nodejs: "nodejs.png",
+    windows: "windows.png",
+    photoshop: "photoshop.jpeg",
+    zoom: "zoom.png",
+    chrome: "chrome.png",
+    office: "office.jpeg",
+    libreoffice: "libreoffice.png",
+    whatsapp: "whatsapp.png",
+    icloud: "icloud.png",
+    git: "git.png",
+    excel: "excel.webp",
+    iphone: "iphone.png",
+    playstation: "playstation.png",
+    nvidia: "nvidia.png",
+    alienware: "alienware.png",
+    core: "core.png",
+    seagate: "seagate.png",
+    samsung: "samsung.png",
+    razer: "razer.png",
+    hp: "hp.png",
+    lenovo: "lenovo.png",
+    twitter: "twitter.png",
+    facebook: "facebook.png",
+    tiktok: "tiktok.png",
+    linkedin: "linkedin.png",
+    pinterest: "pinterest.png",
+    snapchat: "snapchat.png",
+    youtube: "youtube.png",
+    oculus: "oculus.png",
+    instagram: "instagram.png",
+    tumblr: "tumblr.png",
+    tony: "tony.webp",
+    thor: "thor.jpeg",
+    spidey: "spidey.jpeg",
+    tchalla: "tchalla.jpeg",
+    hulk: "hulk.png",
+    strange: "strange.png",
+    cyclo: "cyclo.webp",
+    loki: "loki.webp",
+    logan: "logan.png",
+    cap: "cap.jpeg",
+    poobear: "poobear.png",
+    tenet: "tenet.jpg",
+
+    //rahul images
+
+    //DC
+    batman: "batman.jpg",
+    superman: "superman.jpg",
+    wonderwoman: "wonderwoman.jpg",
+    flash: "flash.jpg",
+    hal: "hal.jpg",
+    aquaman: "aquaman.jpg",
+    joker: "joker.jpg",
+    zatanna: "zatanna.jpg",
+    catwoman: "catwoman.jpg",
+    dickgrayson: "dickgrayson.jpg",
+
+    //Anime
+    naruto: "naruto.jpg",
+    luffy: "luffy.jpg",
+    light: "light.jpg",
+    edward: "edward.jpg",
+    eva: "eva.jpg",
+    sakura: "sakura.jpg",
+    yusuke: "yusuke.jpg",
+    goku: "goku.jpg",
+    zoro: "zoro.jpg",
+    spike: "spike.jpg",
+
+    //Naruto
+    naruto: "naruto.jpg",
+    sasuke: "sasuke.jpg",
+    hinata: "hinata.jpg",
+    jiraya: "jiraya.jpg",
+    kankuro: "kankuro.jpg",
+    deidara: "deidara.jpg",
+    guy: "guy.jpg",
+    katsuya: "katsuya.jpg",
+    shukaku: "shukaku.jpg",
+    kakashi: "kakashi.jpg",
+    
+    //onepiece
+    luffy:"luffy.jpg",
+    zoro:"zoro.jpg",
+    nami:"nami.jpg",
+    sanji:"sanji.jpg",
+    usopp:"usopp.jpg",
+    robin:"robin.jpg",
+    chopper:"chopper.jpg",
+    brook:"brook.jpg",
+    franky:"franky.jpg",
+    jinbe:"jinbe.jpg",
+
+    //NBA
+    lebron:"lebron.jpg",
+    curry:"curry.jpg",
+    giannis:"giannis.jpg",
+    harden:"harden.jpg",
+    embiid:"embiid.jpg",
+    porzingis:"porzingis.jpg",
+    chris:"chris.jpg",
+    davis:"davis.jpg",
+    westbrook:"westbrook.jpg",
+    jokic:"jokic.jpg",
+
+    //FOOD
+    lasgna:"lasgna.jpg",
+    sushi:"sushi.jpg",
+    nachos:"nachos.jpg",
+    pbj:"pbj.jpg",
+    brie:"brie.jpg",
+    carbonora:"carbonora.jpg",
+    chowmein:"chowmein.jpg",
+    roti:"roti.jpg",
+    paella:"paella.jpg",
+    biryani:"biryani.jpg"
+
+
+
+    // Add more word-picture mappings as needed
+};
+
+function generateRandomSequence(min, max) {
+    const sequence = [];
+  
+    // Create an array with numbers from min to max
+    for (let i = min; i <= max; i++) {
+      sequence.push(i);
+    }
+  
+    // Shuffle the array to randomize the sequence
+    for (let i = sequence.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [sequence[i], sequence[j]] = [sequence[j], sequence[i]];
+    }
+  
+    return sequence;
+  }
+  
+  const minNumber = 0;
+  const maxNumber = 9;
+  
+  let randomSequence = generateRandomSequence(minNumber, maxNumber);
+  
+  //console.log(randomSequence); // This will print a random sequence of numbers from 1 to 10
+  
+// Function to update the word picture
+function updateWordPicture(word) {
+    const imgElement = document.querySelector(".word-picture");
+    if (wordToPicture[word]) {
+        imgElement.src = `pictures/${wordToPicture[word]}`;
+        imgElement.alt = `Picture for ${word}`;
+    } else {
+        imgElement.src = ""; // Clear the image if there's no corresponding picture
+        imgElement.alt = "Word Picture";
+    }
+}
+
+var url_string = window.location.href.toString(); 
+var url = new URL(url_string);
+var i = url.searchParams.get("index");
+debugger;
+
+let currentWordList = [];
+var index_increase=0;
+let wordIndex = randomSequence[index_increase]; // Initialize word index
+let score = 0;
+
+let word, maxGuesses, incorrectLetters = [], correctLetters = [];
+
+function randomWord() {
+    if ( index_increase >= currentWordList.length) {
+        // All words have been asked, show total score
+        let msg = `Congratulations! You have completed the game. Total Score: ${score}/10`;
+        popupUP(msg, true,2,"Game Over");
+        //alert(`Congratulations! You have completed the game. Total Score: ${score}`);
+        return;
+      }
+    
+    let ranItem = currentWordList[wordIndex];
+    // wordList.splice(randomIndex, 1);
+    word = ranItem.word;
+    maxGuesses = word.length >= 5 ? 8 : 6;
+    correctLetters = []; incorrectLetters = [];
+    hintTag.innerText = ranItem.hint;
+    guessLeft.innerText = maxGuesses;
+    wrongLetter.innerText = incorrectLetters;
+
+
+    updateWordPicture(word);
+    
+    let html = "";
+    for (let i = 0; i < word.length; i++) {
+        html += `<input type="text" disabled>`;
+        inputs.innerHTML = html;
+    }
+}
+
+function initGame(e) {
+    let key = e.target.value.toLowerCase();
+    if(key.match(/^[A-Za-z]+$/) && !incorrectLetters.includes(` ${key}`) && !correctLetters.includes(key)) {
+        if(word.includes(key)) {
+            for (let i = 0; i < word.length; i++) {
+                if(word[i] == key) {
+                    correctLetters += key;
+                    inputs.querySelectorAll("input")[i].value = key;
+                }
+            }
+        } else {
+            maxGuesses--;
+            incorrectLetters.push(` ${key}`);
+        }
+        guessLeft.innerText = maxGuesses;
+        wrongLetter.innerText = incorrectLetters;
+    }
+    typingInput.value = "";
+
+
+    setTimeout(() => {
+        if(correctLetters.length === word.length) {
+            //alert(`Congrats! You found the word ${word.toUpperCase()}`);
+            let msg=`Congrats! You found the word ${word.toUpperCase()}`;
+            
+            popupUP(msg,false);
+            score++; 
+            index_increase++;
+            wordIndex=randomSequence[index_increase]; // Move to the next word
+        // randomSequence.slice(0)
+        
+        randomWord();
+            
+
+        } else if(maxGuesses < 1) {
+            let msg = "Incorrect Answer. You don't have remaining guesses";
+            debugger;
+           popupUP(msg, false,999,"Incorrect");
+            //alert("Game over! You don't have remaining guesses");
+            for(let i = 0; i < word.length; i++) {
+                inputs.querySelectorAll("input")[i].value = word[i];
+
+            }
+            index_increase++;
+            wordIndex=randomSequence[index_increase]; // Move to the next word
+            randomWord();
+        }
+    }, 100);
+}
+function popupUP(popdata, eflag,pflag=1,head_str="Correct"){
+    
+    
+    $('#modal_content').html(popdata);
+    $('#val-mess').html(head_str);
+    if(pflag===1){
+      $('.wrong-img').hide();
+      $('.correct-img').show();
+      $('.game-over-img').hide();
+    }else if(pflag===2){
+      $('.wrong-img').hide();
+      $('.correct-img').hide();
+      $('.game-over-img').show();
+    }else{
+      $('.wrong-img').show();
+      $('.correct-img').hide();
+      $('.game-over-img').hide();
+    }
+    if(eflag){
+    $('#exampleModal .modal-footer .startB').css("display", "block");
+    $('#exampleModal .modal-footer .closeB').css("display", "none");
+    }
+    // Use Bootstrap's JavaScript API to open the modal
+    $('#exampleModal').modal('show');
+
+
+}
+function closeBootstrapModal() {
+    // Use Bootstrap's JavaScript API to close the modal
+    $('#myModal').modal('hide');
+
+    // Remove the modal element from the document's body
+    const modalElement = document.getElementById("myModal");
+    if (modalElement) {
+        document.body.removeChild(modalElement);
+    }
+}
+
+if (i == 1) {
+    currentWordList = wordList1;
+  } else if (i == 2) {
+    currentWordList = wordList2;
+  } else if (i == 3) {
+    currentWordList = wordList3;
+  }else if (i == 4) {
+    currentWordList = wordList4;
+  }else if (i == 5) {
+    currentWordList = wordList5;
+  }else if (i == 6) {
+    currentWordList = wordList6;
+  }else if (i == 7) {
+    currentWordList = wordList7;
+  }else if (i == 8) {
+    currentWordList = wordList8;
+  }else if ( i==9 ){
+    currentWordList = wordList9
+  }else if (i == 10) {
+    currentWordList = wordList10;
+  }else if (i == 11) {
+    currentWordList = wordList11;
+  }
+
+
+  randomWord();
+  resetBtn.addEventListener("click", () => {
+    // score = 0;
+    // wordIndex = 0;
+    // currentWordList = [];
+    //  // Reset the current word list
+    // // Choose the appropriate word list based on index (i)
+    // if (i == 1) {
+    //   currentWordList = wordList1;
+    // } else if (i == 2) {
+    //   currentWordList = wordList2;
+    // } else if (i == 3) {
+    //   currentWordList = wordList3;
+    // }
+    // randomWord(); // Start the game again
+    window.location.reload();
+  });
+typingInput.addEventListener("input", initGame);
+inputs.addEventListener("click", () => typingInput.focus());
+document.addEventListener("keydown", () => typingInput.focus());
---
+++ js/words.js
@@ -0,0 +1,522 @@
+let wordList1 = [
+    {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "c",
+        hint: "Stacks and heaps are part of its memory management"
+        
+    },
+    {
+        word: "typescript",
+        hint: "JavaScript's main competitor in web scripting"
+        
+    },
+    {
+        word: "r",
+        hint: "Popular language for data analysis and statistics"
+        
+    },
+    {
+        word: "swift",
+        hint: "Programming language for creating ios applications"
+        
+    },
+    {
+        word: "java",
+        hint: "Developed by Sun Microsystems, now owned by Oracle"
+        
+    },
+    {
+        word: "modular",
+        hint: "A concept that refers to breaking a complex problems into smaller, more manageable parts"
+        
+    },
+    {
+        word: "ruby",
+        hint: "Scripting language with a focus on simplicity"
+        
+    },
+    {
+        word: "haskell",
+        hint: "Functional language known for its strong typing"
+        
+    },
+    {
+        word: "nodejs",
+        hint: "Used extensively for web development, especially on the server-side"
+        
+    },]
+    
+let wordList2=[ 
+    {
+        word: "windows",
+        hint: "Popular operating system by Microsoft"
+    },
+    {
+        word: "photoshop",
+        hint: "Adobe's photo editing software"
+        
+    },
+    {
+        word: "zoom",
+        hint: "Video conferencing software"
+        
+    },
+    {
+        word: "chrome",
+        hint: "Web browser developed by Google"
+        
+    },
+    {
+        word: "office",
+        hint: "Microsoft's suite of office productivity software"
+        
+    },
+    {
+        word: "libreoffice",
+        hint: "Open-source office suite with a penguin mascot"
+        
+    },
+    {
+        word: "whatsapp",
+        hint: "Messaging app with a green logo, now owned by Facebook"
+        
+    },
+    {
+        word: "icloud",
+        hint: "Cloud storage service by Apple"
+        
+    },
+    {
+        word: "git",
+        hint: "Version control system for code collaboration"
+        
+    },
+    {
+        word: "excel",
+        hint: "spreadsheet application that is part of the Microsoft Office suite"
+        
+    },]
+
+let wordList3=[
+    {
+        word: "iphone",
+        hint: "Apple's flagship smartphone"
+    },
+    {
+        word: "playstation",
+        hint: "Gaming console by Sony, known for its exclusives"
+        
+    },
+    {
+        word: "nvidia",
+        hint: "Leading manufacturer of graphics processing units"
+        
+    },
+    {
+        word: "alienware",
+        hint: "High-end gaming PC manufacturer"
+        
+    },
+    {
+        word: "core",
+        hint: "Popular line of processors by Intel"
+        
+    },
+    {
+        word: "seagate",
+        hint: "Leading manufacturer of computer hard drives"
+        
+    },
+    {
+        word: "samsung",
+        hint: "South Korean electronics giant, known for phones and TVs"
+        
+    },
+    {
+        word: "razer",
+        hint: "Gaming hardware company known for its mice and keyboards"
+        
+    },
+    {
+        word: "hp",
+        hint: "American multinational corporation known for printers"
+        
+    },
+    {
+        word: "lenovo",
+        hint: "Chinese multinational company, the world's largest PC vendor"
+        
+    },]
+
+    let wordList4=[{
+        word: "twitter",
+        hint: "A platform known for its 140-character limit"
+    },
+    {
+        word: "facebook",
+        hint: "Owned by Mark Zuckerberg, it's the largest social network"
+        
+    },
+    {
+        word: "tiktok",
+        hint: "Short-form video app that gained popularity in 2020"
+        
+    },
+    {
+        word: "linkedin",
+        hint: "Professional networking platform for job seekers"
+        
+    },
+    {
+        word: "pinterest",
+        hint: "Image-centric platform with pins and boards"
+        
+    },
+    {
+        word: "snapchat",
+        hint: "Popular messaging app known for its disappearing messages"
+        
+    },
+    {
+        word: "youtube",
+        hint: "Video-sharing platform where creators can earn money"
+        
+    },
+    {
+        word: "oculus",
+        hint: "Owned by Meta Platforms, it's a virtual reality social platform"
+        
+    },
+    {
+        word: "instagram",
+        hint: "Photo and video sharing app with Stories feature"
+        
+    },
+    {
+        word: "tumblr",
+        hint: "Microblogging platform focused on multimedia content"
+        
+    },]
+
+let wordList5=[
+    {
+        word: "tony",
+        hint: "Billionaire genius with a suit"
+    },
+    {
+        word: "thor",
+        hint: "Mighty god of thunder"
+        
+    },
+    {
+        word: "spidey",
+        hint: "Red and blue wall-crawler"
+        
+    },
+    {
+        word: "tchalla",
+        hint: "Wakandan superhero and king"
+        
+    },
+    {
+        word: "hulk",
+        hint: "Green, gamma-powered giant"
+        
+    },
+    {
+        word: "strange",
+        hint: "Sorcerer Supreme"
+        
+    },
+    {
+        word: "cyclo",
+        hint: "Mutant with optic blasts"
+        
+    },
+    {
+        word: "loki",
+        hint: "Norse trickster and mischief-maker"
+        
+    },
+    {
+        word: "logan",
+        hint: "Feline superheroine with claws"
+        
+    },
+    {
+        word: "cap",
+        hint: "Super-soldier from World War II"
+        
+    },]
+
+
+//Rahul's words 
+
+//DC
+let wordList6=[
+    {
+        word: "batman",
+        hint: "The Dark Knight"
+    },
+    {
+        word: "superman",
+        hint: "Man of Steel"
+    },
+    {
+        word: "wonderwoman",
+        hint: "Amazonian warrior princess"
+    },
+    {
+        word: "flash",
+        hint: "Speedster in a red suit"
+    },
+    {
+        word: "hal",
+        hint: "Green Lantern with willpower"
+    },
+    {
+        word: "aquaman",
+        hint: "King of Atlantis"
+    },
+    {
+        word: "joker",
+        hint: "The Clown Prince of Crime"
+    },
+    {
+        word: "zatanna",
+        hint: "Mistress of Magic"
+    },
+    {
+        word: "catwoman",
+        hint: "Gotham's cat burglar"
+    },
+    {
+        word: "dickgrayson",
+        hint: "Boy Wonder"
+    },
+]
+
+
+//ANIME
+
+let wordList7=[
+    {
+        word: "naruto",
+        hint: "7th Hokage"
+    },
+    {
+        word: "luffy",
+        hint: "Pirate captain with a straw hat"
+    },
+    {
+        word: "light",
+        hint: "Shinigami who loves apples"
+    },
+    {
+        word: "edward",
+        hint: "Alchemist searching for the Philosopher's Stone"
+    },
+    {
+        word: "eva",
+        hint: "Giant robot pilot"
+    },
+    {
+        word: "sakura",
+        hint: "Magical girl with pink hair"
+    },
+    {
+        word: "yusuke",
+        hint: "Spirit detective with a green jacket"
+    },
+    {
+        word: "goku",
+        hint: "Saiyan warrior from Planet Vegeta"
+    },
+    {
+        word: "zoro",
+        hint: "Pirate swordsman with green hair"
+    },
+    {
+        word: "spike",
+        hint: "Bounty hunter in a yellow jumpsuit"
+    },
+]
+
+//NARUTO
+let wordList8=[
+    {
+        word: "naruto",
+        hint: "7th Hokage"
+    },
+    {
+        word: "sasuke",
+        hint: "Sharingan user and last Uchiha"
+    },
+    {
+        word: "hinata",
+        hint: "Queen of huga clan"
+    },
+    {
+        word: "jiraya",
+        hint: "Legendary Sannin with white hair"
+    },
+    {
+        word: "kankuro",
+        hint: "Puppet master from the Sand Village"
+    },
+    {
+        word: "deidara",
+        hint: "Akatsuki member with clay explosives"
+    },
+    {
+        word: "guy",
+        hint: "Master of the Eight Gates"
+    },
+    {
+        word: "katsuya",
+        hint: "Tsunade's slug summon"
+    },
+    {
+        word: "shukaku",
+        hint: "Tailed Beast sealed within Gaara"
+    },
+    {
+        word: "kakashi",
+        hint: "Anbu captain with a mask"
+    },
+]
+
+//ONEPEICE
+let wordList9=[
+    {
+        word: "luffy",
+        hint: "Pirate captain with a straw hat"
+    },
+    {
+        word: "zoro",
+        hint: "Pirate swordsman with green hair"
+    },
+    {
+        word: "nami",
+        hint: "Navigator and cartographer"
+    },
+    {
+        word: "sanji",
+        hint: "strawhat crew cook"
+    },
+    {
+        word: "usopp",
+        hint: "long nose sniper"
+    },
+    {
+        word: "robin",
+        hint: "multiple hands historian"
+    },
+    {
+        word: "chopper",
+        hint: "reindeer doctor"
+    },
+    {
+        word: "brook",
+        hint: "musician with skeleton body"
+    },
+    {
+        word: "franky",
+        hint: "cyborg"
+    },
+    {
+        word: "jinbe",
+        hint: "Fish-man warrior with tattoos"
+    },
+]
+
+//NBA
+let wordList10=[
+    {
+        word: "lebron",
+        hint: "The King of the Lakers"
+    },
+    {
+        word: "curry",
+        hint: "Splash Brother with a deadly three-point shot."
+    },
+    {
+        word: "giannis",
+        hint: "The Greek Freak from the Bucks"
+    },
+    {
+        word: "harden",
+        hint: "The Beard and former Rocket"
+    },
+    {
+        word: "embiid",
+        hint: "The Process in Philadelphia"
+    },
+    {
+        word: "porzingis",
+        hint: "The Unicorn from Latvia"
+    },
+    {
+        word: "chris",
+        hint: "CP3 and Point God"
+    },
+    {
+        word: "davis",
+        hint: "The Brow and Lakers' big man"
+    },
+    {
+        word: "westbrook",
+        hint: " Mr.Triple-Doublein Washington"
+    },
+    {
+        word: "jokic",
+        hint: "The Joker from Denver."
+    },
+]
+
+//FOOD
+let wordList11=[
+    {
+        word: "lasgna",
+        hint: "Italian dish with layers of pasta, cheese, and sauce"
+    },
+    {
+        word: "sushi",
+        hint: "Japanese dish of vinegared rice with seafood and vegetables"
+    },
+    {
+        word: "nachos",
+        hint: "Mexican tortilla dish with cheese and various toppings"
+    },
+    {
+        word: "pbj",
+        hint: "Classic American sandwich with peanut butter and jelly"
+    },
+    {
+        word: "brie",
+        hint: "Creamy French cheese often used in desserts"
+    },
+    {
+        word: "carbonora",
+        hint: "Italian pasta dish with bacon, eggs, and cheese"
+    },
+    {
+        word: "chowmein",
+        hint: "Popular Chinese dish made with stir-fried noodles"
+    },
+    {
+        word: "roti",
+        hint: "Indian flatbread made from chickpea flour"
+    },
+    {
+        word: "paella",
+        hint: "Spanish rice dish often flavored with saffron"
+    },
+    {
+        word: "biryani",
+        hint: "Indian dish of rice with meat or vegetables, flavored with saffron or turmeric"
+    },
+]
